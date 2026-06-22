df0be1b4a153a3094f545743dd53f94de83745af
CNTT <qlinhphan@gmail.com>
2026-06-22 11:11:34 +0700

    change port

--- back/api.py
+++ back/api.py
@@ -92,7 +92,8 @@ app = FastAPI(title="Medical RAG API", version="1.0.0", lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
+        # "http://localhost:5173",
+        "*",
     ],
     allow_credentials=True,
     allow_methods=["*"],
